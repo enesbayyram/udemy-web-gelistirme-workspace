509243b6d15c43ce8acae7b385620fc479ebde2f
Enes Bayram <enesbayram166@gmail.com>
2023-05-02 11:57:46 +0300

    xx

--- Udemy Javascript Workspace/2.JavaScript Dahil Etme/app.js
+++
@@ -1,9 +0,0 @@
-
-function getStudents(){
-    fetch("http://localhost:8080/students")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.log("Hata oluştu ", err));
-}
-
-getStudents();
